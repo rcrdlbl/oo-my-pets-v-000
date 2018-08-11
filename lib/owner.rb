34377a64aea8c3c6e27787ce2bc68b12ab0932d9
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-11 00:57:00 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -17,4 +17,12 @@ class Owner
   def buy_fish(name)
     @pets[:fishes] << Fish.new(name)
   end
+
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name)
+  end
+
+  def buy_cat(name)
+    @pets[:cats] << Cat.new(name)
+  end
 end
